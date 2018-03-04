4dd975a60497c1c44ebc4caf946f310ff787c4eb
cao1 <cao1@stolaf.edu>
2018-03-04 14:03:33 -0600

    added cache

--- screens/active.js
+++ screens/active.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import {Image, Text, StyleSheet, View, Dimensions, ScrollView} from 'react-native';
+import {Image, AsyncStorage, Text, StyleSheet, View, Dimensions, RefreshControl, ScrollView} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import {Button} from 'react-native-elements';
 import Expo, {Audio} from 'expo';
 import ScrollViewItem from './ScrollViewItem.js';
 const soundObject = new Expo.Audio.Sound();
+const info = [];
 const audio = "https://mixtapemonkey.com/mixtapes/zip/637/Chance%20The%20Rapper-%20Acid%20Rap/01.%20Good%20Ass%20Intro%20(Prod.%20by%20Peter%20Cottontale,%20Cam%20for%20J.U.S.T.I.C.E%20League%20&%20Stefan%20Ponce)%20.mp3";
 export default class active extends React.Component {
  constructor(props){
@@ -12,6 +13,9 @@ export default class active extends React.Component {
  this.state = {
   refreshing:false,
   sourceData:[],
+  loaded:false,
+  status:[],
+  percentage:0,
  }; 
  }
  static navigationOptions = {
@@ -32,13 +36,13 @@ export default class active extends React.Component {
     }
   }
 
+
   _stopSound(){
     soundObject.stopAsync();
     soundObject.unloadAsync();
   }
 
-  componentDidMount=()=> {
-    this._playSound(audio);
+  _onRefresh(){
     this.setState({refreshing:true});
     return fetch('https://plgaia-staging.herokuapp.com/api/v1/post_get_active/4Wa0y74X1mAKKIo2qgiWii',{
       method:"GET", 
@@ -49,32 +53,66 @@ export default class active extends React.Component {
       this.setState({
         refreshing:false,
         sourceData: data.get_active,
+      },function(){
+        return AsyncStorage.setItem("info",JSON.stringify(this.state.sourceData))
+        .then(json => console.log("success!"))
+        .catch(error=>console.log("error!"))
       }
       )
-      console.log(this.state.sourceData);
+      //console.log(this.state.sourceData);
     })
     .catch((error)=>{
       console.error(error);
     });
+    //this._getInfo();
   }
 
-  componentWillUnmount=()=>{this._stopSound();}
 
   renderItems(item,i){
     return <ScrollViewItem key={i} detail={item}/>;
   }
 
+  _getInfo = async()=>{
+    try{
+      console.log("get called");
+      info = JSON.parse(await AsyncStorage.getItem('info'));
+      console.log("in function");
+      if(!info) {
+        this._onRefresh();
+      }
+
+    }catch(error){
+      console.log(error);
+    }
+    
+  }
+
+  componentDidMount=()=> {
+    this._playSound(audio);
+   
+    
+}
+
+  componentWillUnmount=()=>{this._stopSound();}
+
+
  render(){
  const { navigate } = this.props.navigation;
  const { params } = this.props.navigation.state;
+ this._getInfo();
  return (
  <View style={styles.container}>
-  <ScrollView>
+  <ScrollView refreshControl={
+      <RefreshControl
+        refreshing = {this.state.refreshing}
+        onRefresh = {this._onRefresh.bind(this)} 
+        />
+    }
+>
   {
-   this.state.sourceData.map((item,i)=>this.renderItems(item,i))
+   info.map((item,i)=>this.renderItems(item,i))
   }
   </ScrollView>
-  </View>
  );
 }
 }
@@ -86,5 +124,6 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
   },
+
 
 });
